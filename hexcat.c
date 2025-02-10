d34ec38a26fc18a06fccac53c7bfee1ceea937a1
Kaamkiya <ziam3.1415@gmail.com>
2025-02-09 21:21:08 -0500

    feat: Add -n flag for no color

--- hexcat.c
+++ hexcat.c
@@ -2,12 +2,11 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "hexcat.h"
 
-char *argv0; /* Required to come before arg.h */
-#include "arg.h"
-
+#define VERSION 0.1
 #define BUFSIZE 16
 
 int usecolor = 1;
@@ -34,33 +33,35 @@ colorize(int ch)
 void
 usage()
 {
-	printf("usage: %s [-hv]\n", argv0);
+	puts("usage: hexcat [-hvnc] [FILE]");
 	exit(1);
 }
 
 int
 main(int argc, char *argv[])
 {
-	ARGBEGIN {
-	case 'h':
-		usage();
-		return 0;
-	case 'v':
-		puts("hexcat v0.1");
-		return 0;
-	} ARGEND;
+	char *pp = NULL;
+
+	while (argc > 2) {
+		pp = argv[1] + (!strncmp(argv[1], "--", 2) && argv[1][2]);
+		if (!strncmp(pp, "-h", 2)) usage();
+		else if (!strncmp(pp, "-n", 2)) usecolor = 0;
+		else if (!strncmp(pp, "-c", 2)) usecolor = 1;
+		else if (!strncmp(pp, "-v", 2)) {
+			fprintf(stderr, "hexcat v%f", VERSION);
+			return 0;
+		}
+		argc--;
+		argv++;
+	}
 
 	FILE *fp = stdin;
-	printf("%d\n", usecolor);
 
 	if (argc > 0) {
-		/*
-		 * argv is consumed by arg.h, so argv0 contains the program name and argv
-		 * contains positional arguments.
-		 */
-		fp = fopen(argv[0], "r");
+		fp = fopen(argv[1], "r");
 		if (fp == NULL) {
-			perror("Failed to open file.");
+			fprintf(stderr, argv[1]);
+			perror("Failed to open file");
 			return 1;
 		}
 	}
@@ -98,7 +99,11 @@ main(int argc, char *argv[])
 
 		for (int i = 0; i < BUFSIZE; i++) {
 			int c = buf[i];
-			printf("\033[1;3%dm%c\033[0m", colorize(c), isprint(c) ? c : '.');
+			if (usecolor) {
+				printf("\033[1;3%dm%c\033[0m", colorize(c), isprint(c) ? c : '.');
+			} else {
+				printf("%c", isprint(c) ? c : '.');
+			}
 		}
 		putchar('\n');
 
