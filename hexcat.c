310246a0057a4d584bb7da5a697baabce00c92c0
Kaamkiya <ziam3.1415@gmail.com>
2025-02-11 20:57:14 -0500

    feat: Add -u flag for uppercase hex printing

--- hexcat.c
+++ hexcat.c
@@ -11,12 +11,10 @@
 # include <io.h>
 #endif
 
-
 #include "hexcat.h"
 
 #define VERSION 0.1
 #define BUFSIZE 16
-
 
 char
 colorize(int ch)
@@ -49,6 +47,7 @@ main(int argc, char *argv[])
 
 	enum Colorize usecolor = ON;
 	int printascii = 1;
+	int uppercase = 0;
 
 	/* Disable colorized output by default for non-TTYs. */
 	if (!isatty(STDOUT_FILENO)) usecolor = OFF;
@@ -59,6 +58,7 @@ main(int argc, char *argv[])
 		else if (!strncmp(arg, "-n", 2)) usecolor = OFF;
 		else if (!strncmp(arg, "-c", 2)) usecolor = FORCE;
 		else if (!strncmp(arg, "-x", 2)) printascii = 0;
+		else if (!strncmp(arg, "-u", 2)) uppercase = 1;
 		else if (!strncmp(arg, "-v", 2)) {
 			fprintf(stderr, "hexcat v%.1f\n", VERSION);
 			return 0;
@@ -96,15 +96,25 @@ main(int argc, char *argv[])
 
 	while ((bytes_read = fread(buf, sizeof(char), BUFSIZE, fp)) > 0) {
 		/* Print an 8-character length, 0-padded long int in hexadecimal form. */
-		fprintf(ofp, "%08lx: ", size);
+		if (uppercase)
+			fprintf(ofp, "%08lX: ", size);
+		else
+			fprintf(ofp, "%08lx: ", size);
 
 		for (int i = 0; i < bytes_read; i++) {
 			int c = buf[i];
 
-			if (usecolor)
-				fprintf(ofp, "\033[1;3%dm%02x\033[0m ", colorize(c), c);
-			else
-				fprintf(ofp, "%02x ", buf[i]);
+			if (usecolor) {
+				if (uppercase)
+					fprintf(ofp, "\033[1;3%dm%02X\033[0m ", colorize(c), c);
+				else
+					fprintf(ofp, "\033[1;3%dm%02x\033[0m ", colorize(c), c);
+			} else {
+				if (uppercase)
+					fprintf(ofp, "%02X ", buf[i]);
+				else
+					fprintf(ofp, "%02x ", buf[i]);
+			}
 
 			if (i == 7)
 				fprintf(ofp, " ");
